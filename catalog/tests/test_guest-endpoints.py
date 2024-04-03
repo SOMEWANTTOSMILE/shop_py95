20be77635aee706870077d85cff90b8af7c518df
SOMEWANTTOSMILE <dead5moon@gmail.com>
2024-04-03 21:54:58 +0300

    Initial add-points_test

--- catalog/tests/test_guest-endpoints.py
+++ catalog/tests/test_guest-endpoints.py
@@ -1,10 +1,30 @@
 import pytest
 from rest_framework.test import APITestCase
+from django.shortcuts import reverse
+from config_test import EVERYTHING_EQUALS_NOT_NONE
 
 
 pytestmark = [pytest.mark.django_db]
 
 
 class TestGuessEndpoints(APITestCase):
-    def test_something(self):
-        assert True == True
+
+    fixtures = ['catalog/tests/fixtures/categories_fixtures.json']
+
+    def test_categories_list_endpoints(self):
+        url = reverse('categories')
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert isinstance(response.data, list)
+        assert response.data == [
+            {
+                "id": 1,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            },
+            {
+                "id": 2,
+                "name": EVERYTHING_EQUALS_NOT_NONE,
+                "description": EVERYTHING_EQUALS_NOT_NONE
+            }
+        ]
